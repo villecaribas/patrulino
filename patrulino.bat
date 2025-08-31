623b934297c2bb8de8eeecab28f49d8de47844e0
ville.medeiros <ville.medeiros@patrulhaeureka.org>
2025-08-31 05:36:35 -0400

    Inicializador do Patrulino com suas bibliotecas

--- patrulino.bat
+++ patrulino.bat
@@ -1,3 +1,3 @@
 start /B c:\PatruLINO\bin\httpd.exe 
 
-start "" "http://localhost/patrulino/snap.html"
+start "" "http://localhost/patrulino/snap.html#open://patrulino.patrulhaeureka.org/libraries/eureka_espacelino.xml"
